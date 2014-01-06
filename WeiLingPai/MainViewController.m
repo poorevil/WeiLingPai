9fecc834ad2226bc3be9deed6e05f6bab4f41493
poorevil <poorevil@gmail.com>
2014-01-06 18:18:57 +0800

    update

---
+++ WeiLingPai/MainViewController.m
@@ -0,0 +1,41 @@
+//
+//  MainViewController.m
+//  WeiLingPai
+//
+//  Created by hanchao on 14-1-6.
+//  Copyright (c) 2014年 evil. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+	[self.navigationItem setTitle:@"微令牌"];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
